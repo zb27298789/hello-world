988c5232a1186e1e71a67ab2f24ec3b2a1f20f98
Zhang Bin <bin.zhang01@sap.com>
2023-05-30 14:15:32 +0800

    added asynceProcess

---
+++ asyncProcess.ts
@@ -0,0 +1,27 @@
+const timeArray = [3000, 2000, 1000];
+function createPromise(time : number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(time);
+        }, time);
+    });
+} 
+
+async function run(t : number) {
+    console.log(await createPromise(t));
+};
+
+
+/* for (const ti of timeArray) {
+    console.log('1111'+ti);
+    run(ti);
+    console.log('2222'+ti);
+} */
+async function test(){ 
+for (const ti of timeArray) {
+    console.log('1111'+ti);
+    console.log(await createPromise(ti));
+    console.log('2222'+ti);
+}
+}
+test();
